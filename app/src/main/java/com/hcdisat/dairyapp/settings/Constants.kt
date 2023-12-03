64dae90ed9b82bc6e67f575ecbf1a4ba4f1eb01e
Hector Caraballo <hcdisat@gmail.com>
2023-12-02 22:35:26 -0500

    - added Constants.kt

---
+++ app/src/main/java/com/hcdisat/dairyapp/settings/Constants.kt
@@ -0,0 +1,6 @@
+package com.hcdisat.dairyapp.settings
+
+object Constants {
+    const val APP_ID = "dairyservice-hlrtc"
+    const val CLIENT_ID = "968840456875-g6lnndc5l8r56in3ts65klt28487qk5v.apps.googleusercontent.com"
+}
