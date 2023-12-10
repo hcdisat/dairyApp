3b61e66164e0d67bedc6f504c72f8a70e4a77f8d
Hector Caraballo <hcdisat@gmail.com>
2023-12-10 13:17:23 -0500

    - deleted RequestState.kt

--- app/src/main/java/com/hcdisat/dairyapp/dataaccess/realm/model/RequestState.kt
+++
@@ -1,8 +0,0 @@
-package com.hcdisat.dairyapp.dataaccess.realm.model
-
-import java.time.LocalDate
-
-sealed interface RequestState {
-    data class Success(val diaries: Map<LocalDate, List<Diary>>) : RequestState
-    data class Error(val throwable: Throwable) : RequestState
-}
