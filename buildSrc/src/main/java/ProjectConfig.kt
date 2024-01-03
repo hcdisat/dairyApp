92c973d5525ea3e9697ef29991d6f87dd31582ac
Hector Caraballo <hcdisat@gmail.com>
2024-01-03 16:32:03 -0500

    - added buildSrc folder

---
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -0,0 +1,15 @@
+object ProjectConfig {
+    const val compileSdk = 34
+    const val minSdk = 24
+    const val targetSdk = 34
+
+    const val versionCode = 1
+    const val versionName = "1.0"
+
+    const val applicationId = "com.hcdisat.dairyapp"
+    const val instrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+    const val jvmTarget = "1.8"
+
+    const val compose = "1.5.1"
+}
