aa4d67d04942f6a0bb3f81dd89e15f9b0c881197
Hector Caraballo <hcdisat@gmail.com>
2024-01-05 16:34:21 -0500

    - removed not needed exceptions

--- core/common/src/main/java/com/hcdisat/common/Exceptions.kt
+++ core/common/src/main/java/com/hcdisat/common/Exceptions.kt
@@ -3,11 +3,11 @@ package com.hcdisat.common
 import android.net.Uri
 
 class UserNotAuthenticatedException : RuntimeException("User is not authenticated")
-class SignInCancelledException : RuntimeException("Sign in operation was cancelled.")
-class UnexpectedException(innerException: Throwable) : RuntimeException(innerException)
+
 data class RealmGenericException(val throwable: Throwable) : RuntimeException(throwable)
 
 class InvalidImageUriException(uri: Uri) : RuntimeException("${uri.path}")
+
 class OperationCanceledException(
     override val message: String = "Operation was Cancelled"
 ) : RuntimeException(message)
